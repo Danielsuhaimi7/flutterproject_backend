8a822c2edac2f3d09589772bb6e38b2d241b74f5
Danielsuhaimi7 <danielsuhaimi7@gmail.com>
2025-06-10 02:39:48 +0800

    loginupdate

--- app.py
+++ app.py
@@ -29,13 +29,18 @@ def login():
 
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE student_id=%s AND password=%s", (student_id, password))
+    cursor.execute("SELECT student_id, role FROM users WHERE student_id=%s AND password=%s", (student_id, password))
     user = cursor.fetchone()
     cursor.close()
     conn.close()
 
     if user:
-        return jsonify({"status": "success", "message": "Login successful"})
+        return jsonify({
+            "status": "success",
+            "message": "Login successful",
+            "student_id": user[0],
+            "role": user[1]
+        })
     else:
         return jsonify({"status": "fail", "message": "Invalid credentials"})
 
